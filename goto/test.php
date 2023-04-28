7fe0a67f075f3147829c56ebf5622e808166a66f
senorpr <66690290+senorpr@users.noreply.github.com>
2023-04-28 19:45:51 -0400

    Update test.php

--- goto/test.php
+++ goto/test.php
@@ -1,3 +1,8 @@
-<?php header( 'Location: https://www.creativefabrica.com/product/sexy-white-t-shirt-mockup/ref/926287/?campaign=T-ShirtMockupDesignsell' ) ; ?>
-
-
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+ <?php header( 'Location: https://www.creativefabrica.com/product/sexy-white-t-shirt-mockup/ref/926287/?campaign=T-ShirtMockupDesignsell' ) ; ?>
+</body>
+</html>
